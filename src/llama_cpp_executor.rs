c1dcbd3facb4439449d1da62a1727fa901f9d39c
Arkitu <85173315+Arkitu@users.noreply.github.com>
2023-12-03 20:12:35 +0100

    ça march omg

--- src/llama_cpp_executor.rs
+++ src/llama_cpp_executor.rs
@@ -1,49 +1,153 @@
-use llm_chain::{traits::{Executor as ExecutorTrait, ExecutorCreationError, ExecutorError}, options::Options, prompt::{Prompt, Data}, output::{Output, Immediate}, tokens::TokenizerError};
-use serde::{Serialize, Deserialize};
+use futures::executor::block_on;
+use llm_chain::{traits::{Executor as ExecutorTrait, ExecutorCreationError, ExecutorError}, options::{Options, OptDiscriminants, Opt}, prompt::{Prompt, Data, ChatRole}, output::Output, tokens::{TokenizerError, Tokenizer as TokenizerTrait, TokenCount, PromptTokensError}};
 use serenity::async_trait;
 
-struct Executor {
+const MAX_TOKENS: usize = 128;
+
+pub struct Executor {
     http: reqwest::Client,
     /// /!\ With no / at the end. ex: "https://127.0.0.1:8080"
     adress: String
 }
 
-#[derive(Serialize)]
-struct ReqCompletion<'a> {
-    prompt: &'a str
-}
+mod req_res {
+    use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize)]
-struct ResCompletion {
-    content: String
-}
+    #[derive(Serialize)]
+    pub struct ReqCompletion<'a> {
+        pub prompt: &'a str,
+        pub stop: &'a Vec<String>
+    }
 
-//#[async_trait]
+    #[derive(Deserialize)]
+    pub struct ResCompletion {
+        pub content: String
+    }
+
+    #[derive(Serialize)]
+    pub struct ReqTokenize<'a> {
+        pub content: &'a str
+    }
+
+    #[derive(Deserialize)]
+    pub struct ResTokenize {
+        pub tokens: Vec<usize>
+    }
+
+    #[derive(Serialize)]
+    pub struct ReqDetokenize {
+        pub tokens: Vec<usize>
+    }
+
+    #[derive(Deserialize)]
+    pub struct ResDetokenize {
+        pub content: String
+    }
+}
+use req_res::*;
+
+#[async_trait]
 impl ExecutorTrait for Executor {
     type StepTokenizer<'a> = Tokenizer<'a>;
 
-    fn new_with_options(options: Options) -> Result<Self, ExecutorCreationError> {
+    fn new_with_options(_: Options) -> Result<Self, ExecutorCreationError> {
         Ok(Self {
             http: reqwest::Client::new(),
-            adress: "https://127.0.0.1:8080".to_string()
+            adress: "http://127.0.0.1:8080".to_string()
         })
     }
 
-    async fn execute(&self, options: &Options, prompt: &Prompt) -> Result<Output, ExecutorError> {
+    async fn execute(&self, opts: &Options, prompt: &Prompt) -> Result<Output, ExecutorError> {
         let prompt = prompt.to_text();
+        let v = Vec::new();
+        let stops = match opts.get(OptDiscriminants::StopSequence) {
+            None => &v,
+            Some(opt) => if let Opt::StopSequence(stops) = opt {
+                stops
+            } else {
+                panic!()
+            }
+        };
+        dbg!(&prompt);
         let res = self.http.post(self.adress.clone() + "/completion").json(&ReqCompletion {
-            prompt: &prompt
+            prompt: &prompt,
+            stop: stops
         }).send().await.unwrap();
         let res: ResCompletion = res.json().await.unwrap();
 
         Ok(Output::new_immediate(Data::Text(res.content)))
     }
 
-    fn get_tokenizer(&self,options: &Options) -> Result<Tokenizer, TokenizerError> {
+    fn tokens_used(
+        &self,
+        options: &Options,
+        prompt: &Prompt,
+    ) -> Result<TokenCount, PromptTokensError> {
+        let tokenizer = self.get_tokenizer(options)?;
+        let input = prompt.to_text();
+        let mut tokens_used = tokenizer
+            .tokenize_str(&input)
+            .map_err(|_e| PromptTokensError::UnableToCompute)?
+            .len() as i32;
+        // includes answer_prefix
+        let answer_prefix = self.answer_prefix(prompt);
+        if let Some(prefix) = answer_prefix {
+            let answer_used = tokenizer
+                .tokenize_str(&prefix)
+                .map_err(|_e| PromptTokensError::UnableToCompute)?
+                .len() as i32;
+            tokens_used += answer_used
+        }
+        let max_tokens = self.max_tokens_allowed(options);
+        Ok(TokenCount::new(max_tokens, tokens_used))
+    }
+
+    fn answer_prefix(&self, prompt: &Prompt) -> Option<String> {
+        if let llm_chain::prompt::Data::Chat(_) = prompt {
+            // Tokenize answer prefix
+            // XXX: Make the format dynamic
+            let prefix = if prompt.to_text().ends_with('\n') {
+                ""
+            } else {
+                "\n"
+            };
+            Some(format!("{}{}:", prefix, ChatRole::Assistant))
+        } else {
+            None
+        }
+    }
+
+    fn max_tokens_allowed(&self, _: &Options) -> i32 {
+        MAX_TOKENS as i32
+    }
+
+    fn get_tokenizer(&self, _: &Options) -> Result<Tokenizer, TokenizerError> {
         Ok(Tokenizer { exec: self })
     }
 }
 
-struct Tokenizer<'a> {
+pub struct Tokenizer<'a> {
     exec: &'a Executor
+}
+
+impl<'a> TokenizerTrait for Tokenizer<'a> {
+    fn tokenize_str(&self, doc: &str) -> Result<llm_chain::tokens::TokenCollection, TokenizerError> {
+        block_on(async move {
+            let res = self.exec.http.post(self.exec.adress.clone() + "/tokenize").json(&ReqTokenize {
+                content: &doc.to_owned()
+            }).send().await.map_err(|_|TokenizerError::TokenizationError)?;
+            let res: ResTokenize = res.json().await.map_err(|_|TokenizerError::TokenizationError)?;
+            Ok(res.tokens.into())
+        })
+    }
+
+    fn to_string(&self, tokens: llm_chain::tokens::TokenCollection) -> Result<String, TokenizerError> {
+        block_on(async move {
+            let res = self.exec.http.post(self.exec.adress.clone() + "/detokenize").json(&ReqDetokenize {
+                tokens: tokens.as_usize()?
+            }).send().await.map_err(|_|TokenizerError::TokenizationError)?;
+            let res: ResDetokenize = res.json().await.map_err(|_|TokenizerError::TokenizationError)?;
+            Ok(res.content)
+        })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use llm_chain::options::ModelRef;
-use llm_chain::{executor, prompt, parameters, options};
-use llm_chain_llama::Executor;
+use llm_chain::options::{Options, Opt};
+use llm_chain::prompt::Prompt;
+use llm_chain::traits::Executor as ExecutorTrait;
+use llm_chain::{prompt, parameters};
+use serenity::builder::{CreateChannel, EditMessage, CreateEmbed, CreateEmbedAuthor};
 use tokio;
 use dotenv::dotenv;
 use std::env;
@@ -10,39 +12,56 @@ use serenity::model::channel::Message;
 use serenity::async_trait;
 
 mod llama_cpp_executor;
+use llama_cpp_executor::Executor;
 
 struct Handler {
-    llm_exec: Executor
+    exec: Executor
 }
 
 impl Handler {
     pub fn new() -> Self {
-        let opts = options!(
-            Model: ModelRef::from_path("./ggml-alpaca-7b-q4.bin"), // Notice that we reference the model binary path
-            ModelType: "llama",
-            MaxContextSize: 512_usize,
-            NThreads: 4_usize,
-            MaxTokens: 0_usize,
-            TopK: 40_i32,
-            TopP: 0.95,
-            TfsZ: 1.0,
-            TypicalP: 1.0,
-            Temperature: 0.8,
-            RepeatPenalty: 1.1,
-            RepeatPenaltyLastN: 64_usize,
-            FrequencyPenalty: 0.0,
-            PresencePenalty: 0.0,
-            Mirostat: 0_i32,
-            MirostatTau: 5.0,
-            MirostatEta: 0.1,
-            PenalizeNl: true,
-            StopSequence: vec!["\n".to_string()]
-        );
-        let exec = executor!(llama, opts).unwrap();
+        // let opts = options!(
+        //     Model: ModelRef::from_path("./ggml-alpaca-7b-q4.bin"), // Notice that we reference the model binary path
+        //     ModelType: "llama",
+        //     MaxContextSize: 512_usize,
+        //     NThreads: 4_usize,
+        //     MaxTokens: 0_usize,
+        //     TopK: 40_i32,
+        //     TopP: 0.95,
+        //     TfsZ: 1.0,
+        //     TypicalP: 1.0,
+        //     Temperature: 0.8,
+        //     RepeatPenalty: 1.1,
+        //     RepeatPenaltyLastN: 64_usize,
+        //     FrequencyPenalty: 0.0,
+        //     PresencePenalty: 0.0,
+        //     Mirostat: 0_i32,
+        //     MirostatTau: 5.0,
+        //     MirostatEta: 0.1,
+        //     PenalizeNl: true,
+        //     StopSequence: vec!["\n".to_string()]
+        // );
+        // let exec = executor!(llama, opts).unwrap();
+        let exec = Executor::new().unwrap();
         Self {
-            llm_exec: exec
+            exec
         }
     }
+}
+
+async fn execute(exec: &Executor, stops: Vec<String>, prompt: String) -> String {
+    let mut opts = Options::builder();
+    opts.add_option(Opt::StopSequence(stops));
+    let opts = opts.build();
+    let prompt = Prompt::Text(
+        prompt
+        //&format!("\n{}: {}\nBot:", msg.author.name, msg.content)
+    );
+    let res = exec.execute(
+        &opts,
+        &prompt
+    ).await.unwrap();
+    res.to_immediate().await.unwrap().to_string()
 }
 
 #[async_trait]
@@ -52,16 +71,71 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.is_own(&ctx.cache) {
+        if msg.is_own(&ctx.cache) || !msg.mentions_me(&ctx.http).await.unwrap() {
             return
         }
+        let mut reply = msg.reply(&ctx.http, "🔄 Réfléchit...").await.unwrap();
+
+        let stops = vec![
+            "SYSTEM:".to_string(),
+            "USER:".to_string()
+        ];
+
+        let content = msg.content_safe(&ctx.cache);
+        let content = match ctx.cache.current_user().discriminator {
+            Some(d) => {
+                let mut d = d.to_string();
+                while d.chars().count() < 4 {
+                    d.insert(0, '0');
+                }
+                content.trim_start_matches(&dbg!(format!("@{}#{}", ctx.cache.current_user().name, d)))
+            },
+            None => content.trim_start_matches(&dbg!(format!("@{}", ctx.cache.current_user().name)))
+        };
+        let content = content.trim();
+
+        let prompt = "\
+            SYSTEM: Tu es AI Bot, un bot discord Français.\
+            Tu es sympa et utilise des emojis.\
+            Tu peux utliser les commandes suivantes en les envoyant dans le chat :\n\
+            - /create_channel \"nom du salon\"\n\
+            Par exemple : \"ASSISTANT: /create_channel general\" crée le salon general\n\
+            - /delete_channel \"nom du salon\"\n\
+            ".to_string() + &format!("USER: {}\nASSISTANT:", content);
+
         println!("Responding to {}…", msg.author.name);
-        let res = prompt!(
-            "System: You are a discord bot assistant, interacting with discord users",
-            &format!("{}: {}", msg.author.name, msg.content)
-        ).run(&parameters!(), &self.llm_exec).await.unwrap();
-        msg.channel_id.say(&ctx.http, res.to_immediate().await.unwrap().to_string()).await.unwrap();
-        println!("Response done for {} !", msg.author.name);
+        let res = execute(&self.exec, stops, prompt).await;
+        let res = res.trim();
+
+        if res.starts_with("/create_channel ") {
+            let res = res.split_at(16).1;
+            let res = res.trim_matches('"');
+            if let Some(guild_id) = msg.guild_id {
+                match guild_id.create_channel(&ctx.http, CreateChannel::new(res)).await {
+                    Err(e) => {
+                        reply.edit(&ctx.http, EditMessage::new().content(format!("❌ Erreur : {}", e))).await.unwrap();
+                    },
+                    Ok(chan) => {
+                        reply.edit(&ctx.http, EditMessage::new().content(format!("✅ <#{}> créé !", chan.id))).await.unwrap();
+                    }
+                }
+            } else {
+                reply.edit(&ctx.http, EditMessage::new().content("❌ Erreur : j'ai essayé de créer un salon mais je ne suis pas dans un serveur")).await.unwrap();
+            }
+        } else {
+            let avatar_url = ctx.cache.current_user().face();
+
+            reply.edit(&ctx.http, EditMessage::new()
+                .content("")
+                .add_embed(
+                    CreateEmbed::new()
+                        .author(CreateEmbedAuthor::new("AI Bot").icon_url(avatar_url))
+                        .description(res)
+                )
+            ).await.unwrap();
+
+            msg.react(&ctx.http, '✅').await.unwrap();
+        }
     }
 }
 
